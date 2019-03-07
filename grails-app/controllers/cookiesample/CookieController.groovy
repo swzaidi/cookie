2103c43c04dc7f1f6072f25d901ca0c15d9c3dc8
Shiraz Zaidi <shiraz.zaidi@care.com>
2019-03-07 15:54:53 +0530

    cookie controller changes

--- grails-app/controllers/cookiesample/CookieController.groovy
+++ grails-app/controllers/cookiesample/CookieController.groovy
@@ -1,21 +1,15 @@
 package cookiesample
 
+import org.grails.web.json.JSONObject
+
 import javax.servlet.http.Cookie
 
 class CookieController {
 
     def index() {
-        String value = "[{\"firstName\":\"Test\",\"max\":\"10\"}]"
-        Cookie cookie = new Cookie("test", value)
-        println "++++++++++ cookie value= " + cookie.getValue()
-        cookie.path = '/'
-        cookie.maxAge = 60 * 60 * 24 * 365
-        //TODO: VRN: we seem to be setting the hostname, which wouldn't work for Akamai proxy
-        //cookie.domain = domain
-        cookie.secure = true
-        cookie.httpOnly = true
-        // set cookie
-        response.addCookie(cookie)
+        def map = [ firstName: 'name', age: 10 ]
+        Cookie c = new Cookie("test", new JSONObject(map).toString() )
+        response.addCookie(c);  // throws exception
         render view:'index'
     }
 }
